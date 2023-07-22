d788ff9c44e2d8044298936955246f816413d290
Farah-halim <level2bis2@gmail.com>
2023-07-22 12:39:10 -0700

    task 1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - Insertion sort algorithm.
+ * @list: A pointer to the head of the list to be sorted.
+*/
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *node, *current, *temp;
+
+	node = (*list)->next;
+
+	while (node)
+	{
+		current = node;
+		while (current->prev && current->prev->n > current->n)
+		{
+			temp = current->prev;
+			current->prev = temp->prev;
+			temp->next = current->next;
+			current->next = temp;
+			temp->prev = current;
+			if (temp->next)
+				temp->next->prev = temp;
+			if (current->prev)
+				current->prev->next = current;
+			else
+				*list = current;
+			print_list(*list);
+		}
+		node = node->next;
+	}
+}
